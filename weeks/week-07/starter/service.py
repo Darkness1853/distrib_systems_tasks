e4bc5cc5f28d53825ad8dcd4b2bb7657112a006a
Darkness1853 <petya.nguen@bk.ru>
2026-03-27 23:02:53 +0700

    WEEK-07

--- weeks/week-07/starter/service.py
+++ weeks/week-07/starter/service.py
@@ -1,19 +1,30 @@
 import grpc
 from concurrent import futures
 # Импортируйте сгенерированные модули
-# import service_pb2
-# import service_pb2_grpc
+import service_pb2
+import service_pb2_grpc
 
-class ServiceImplementation: # Унаследуйтесь от сгенерированного Servicer
-    pass
-    # Реализуйте методы сервиса
+product_db = {}
 
-def serve():
+class ProductServiceImplementation(service_pb2_grpc.ProductsServiceServicer): # Унаследуйтесь от сгенерированного Servicer
+    
+    def CreateProduct(self, request, context):
+        product = service_pb2.Product(
+            id=request.id,
+            price=request.price
+        )
+        product_db[request.id] = product
+        
+        print(f"Product: ID={product.id}, Price={product.price}")
+        
+        return product
+
+def server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # service_pb2_grpc.add_MyServiceServicer_to_server(ServiceImplementation(), server)
+    service_pb2_grpc.add_ProductsServiceServicer_to_server(ProductServiceImplementation(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    serve()
+    server()
